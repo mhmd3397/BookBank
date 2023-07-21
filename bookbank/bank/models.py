9bb8d89b668a47a496bae3c19c8e68f2bce0dde9
mhmd3397 <1142456@student.birzeit.edu>
2023-07-21 20:04:42 +0300

    Update models.py

--- bookbank/bank/models.py
+++ bookbank/bank/models.py
@@ -46,19 +46,65 @@ class UserManager(models.Manager):
             errors['password'] = "Password should be at least 8 characters."
         elif password != confirm_password:
             errors['confirm_password'] = "Passwords do not match."
-        # Validate Birthday (if provided)
-        birthday = postData.get('birthday')
-        if birthday:
-            try:
-                birthday_date = datetime.strptime(birthday, '%Y-%m-%d').date()
-                today = date.today()
-                if (today - birthday_date).days < 0:
-                    errors['birthday'] = "Birthday cannot be in the future."
-                elif calculate_age(birthday_date) < 13:
-                    errors['birthday'] = "You must be at least 13 years old to register."  # noqa
-            except ValueError:
-                errors['birthday'] = "Invalid date format."
-        return errors
+        # # Validate Birthday (if provided)
+        # birthday = postData.get('birthday')
+        # if birthday:
+        #     try:
+        #         birthday_date = datetime.strptime(birthday, '%Y-%m-%d').date()
+        #         today = date.today()
+        #         if (today - birthday_date).days < 0:
+        #             errors['birthday'] = "Birthday cannot be in the future."
+        #         elif calculate_age(birthday_date) < 13:
+        #             errors['birthday'] = "You must be at least 13 years old to register."  # noqa
+        #     except ValueError:
+        #         errors['birthday'] = "Invalid date format."
+        # return errors
+
+
+class EmployeeManager(models.Manager):
+    def basic_validator_registration(self, postData):
+        errors = {}
+        # Validate First Name
+        first_name = postData.get('first_name')
+        if not first_name:
+            errors['first_name'] = "First name is required."
+        elif len(first_name) < 2:
+            errors['first_name'] = "First name should be at least 2 characters."  # noqa
+        # Validate Last Name
+        last_name = postData.get('last_name')
+        if not last_name:
+            errors['last_name'] = "Last name is required."
+        elif len(last_name) < 2:
+            errors['last_name'] = "Last name should be at least 2 characters."
+        # Validate Email
+        EMAIL_REGEX = re.compile(
+            r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        email = postData.get('email')
+        if not email:
+            errors['email'] = "Email is required."
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email format."
+        else:
+            # Check if a user with the same email already exists
+            existing_user = User.objects.filter(email=email).first()
+            if existing_user:
+                errors['email'] = "A user with this email already exists."
+        # Validate Password
+        password = postData.get('password')
+        confirm_password = postData.get('confirm_password')
+        if not password:
+            errors['password'] = "Password is required."
+        elif len(password) < 8:
+            errors['password'] = "Password should be at least 8 characters."
+        elif password != confirm_password:
+            errors['confirm_password'] = "Passwords do not match."
+        # validate Employee_id
+        employee_id_REGEX = re.compile(r"^[A-Z\d]{1}[A-Z\d._/\-]{49}$")
+        employee_id = postData.get('employee_id')
+        if not employee_id:
+            errors['employee_id'] = "employee id is required."
+        elif not employee_id_REGEX.match(postData['employee_id']):
+            errors['employee_id'] = "Invalid employee id."
 
     def basic_validator_login(self, postData):
         errors = {}
@@ -66,13 +112,13 @@ class UserManager(models.Manager):
         email = postData.get('email')
         existing_user = User.objects.filter(email=email).first()
         if not existing_user:
-            errors['email'] = "A user not found."
+            errors['email'] = "Your email or password is incorrect."
         # Validate password
         user = User.objects.filter(email=email)
         if user:
             logged_user = user[0]
             if not bcrypt.checkpw(postData.get('password').encode(), logged_user.password.encode()):  # noqa
-                errors['password'] = "Your password is incorrect."
+                errors['password'] = "Your email or password is incorrect."
         return errors
 
 
@@ -106,6 +152,8 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
 
 
 class Employee(models.Model):
@@ -116,6 +164,7 @@ class Employee(models.Model):
     employee_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = EmployeeManager()
 
 
 class Appointment(models.Model):
