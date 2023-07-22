6673bdd987ccbb4e4085dc60a9d1bc0e54caa4db
Azzam97 <110722912+Azzam97@users.noreply.github.com>
2023-07-22 16:28:08 +0300

    created the home_page_customer.html with links

--- bookbank/bank/urls.py
+++ bookbank/bank/urls.py
@@ -4,9 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.main_page),
     path('employee_registration/', views.employee_registration_view,
-         name='employee_registration'),
+          name='employee_registration'),
     path('customer_registration/', views.customer_registration_view,
-         name='customer_registration'),
+          name='customer_registration'),
     path('login_page/', views.login,
-         name='login_registration'),
+          name='login_registration'),
+    path('home_page_customer/', views.home_page_customer,
+         name ='home_page_customer'),
+    path('appointment_detail/<int:id>', views.appointment_details,
+         name='appintment_details'),
+    path('edit/<int:id>', views.edit),
+    path('delete/<int:id>', views.delete),
+    path('logout/', views.logout, name='logout')
 ]
--- bookbank/bank/views.py
+++ bookbank/bank/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import User, Employee, Appointment
 
 # Create your views here.
 
@@ -17,3 +18,31 @@ def customer_registration_view(request):
 
 def login(request):
     return render(request, 'login_page.html')
+
+
+def home_page_customer(request):
+    # context = {
+    #     'user': request.session['user'],
+    #     'message': request.session['message']
+    # }
+    # #this can be updated after implementing the other methods
+    return render(request, 'home_page_customer.html', ) #context needs to be added
+
+
+def appointment_details(request, id):
+    return render(request, 'home_page_customer.html', )
+
+
+def edit(request, id):
+    #get the appointment from the database using the id
+    return render(request, "edit.html")
+
+
+def delete(request, id):
+    # appointment = Appointment.objects.get(id=id)
+    # appointment.delete()
+    return redirect('home_page_customer')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
